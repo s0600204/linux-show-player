4d0f6ab90a98d86e202dd25aa5eec6c5379f81e5
s0600204 <dev@s0600204>
2020-09-17 02:47:19 +0100

    Implement signals on Cart Page add, remove, and rename

--- lisp/plugins/cart_layout/tab_widget.py
+++ lisp/plugins/cart_layout/tab_widget.py
@@ -1,6 +1,6 @@
 # This file is part of Linux Show Player
 #
-# Copyright 2018 Francesco Ceruti <ceppofrancy@gmail.com>
+# Copyright 2023 Francesco Ceruti <ceppofrancy@gmail.com>
 #
 # Linux Show Player is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@ from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtWidgets import QTabWidget
 
+from lisp.core.signal import Signal
 from lisp.ui.widgets.qeditabletabbar import QEditableTabBar
 
 
@@ -35,6 +36,9 @@ class CartTabWidget(QTabWidget):
 
         self.setFocusPolicy(Qt.StrongFocus)
         self.setAcceptDrops(True)
+
+        self.page_renamed = Signal()
+        self.tabBar().textChanged.connect(self.page_renamed.emit)
 
     def dragEnterEvent(self, event):
         if event.mimeData().text() == CartTabWidget.DRAG_MAGIC:
@@ -61,9 +65,13 @@ class CartTabWidget(QTabWidget):
 
         return texts
 
+    def setTabText(self, index, label):
+        super().setTabText(index, label)
+        self.page_renamed.emit(index, label)
+
     def setTabTexts(self, texts):
         for i, text in enumerate(texts):
-            self.tabBar().setTabText(i, text)
+            self.setTabText(i, text)
 
     def pages(self):
         for index in range(self.count()):
