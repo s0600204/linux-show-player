db53dc9dacc016c218686d27f769c6e0fa395583
s0600204 <dev@s0600204>
2021-08-18 16:54:40 +0100

    Resolve path issues wrt media files
    It's now possible to play media files on Windows, and also see details about them (e.g. their duration)

--- lisp/plugins/gst_backend/gst_properties.py
+++ lisp/plugins/gst_backend/gst_properties.py
@@ -1,6 +1,6 @@
 # This file is part of Linux Show Player
 #
-# Copyright 2021 Francesco Ceruti <ceppofrancy@gmail.com>
+# Copyright 2023 Francesco Ceruti <ceppofrancy@gmail.com>
 #
 # Linux Show Player is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
